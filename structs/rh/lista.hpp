e0fc78c9767e33d06eec9a9b8ece165a7a774df6
wellingtondellamura <della.mura@gmail.com>
2016-09-27 10:14:59 -0300

    Exemplo RH

---
+++ structs/rh/lista.hpp
@@ -0,0 +1,14 @@
+#ifndef LISTA_H_
+#define LISTA_H_
+
+#include "dados.hpp"
+
+#define MAX_SIZE 1000
+
+struct Lista_func{
+    int size = 0;
+    Funcionario items[MAX_SIZE];
+};
+
+
+#endif /* LISTA_H_ */
