38959af13d5bb8bab575fb816f4dad85d55bf2b7
wellingtondellamura <della.mura@gmail.com>
2016-10-03 10:47:39 -0300

    RH

---
+++ memory/allocation/test2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int main(){
+    int* ptr = NULL;
+    int max = 100000000;
+    srand(time(NULL));
+    // cout << "Quantos números deseja sortear?" << endl;
+    // cin >> max;
+    ptr = (int*) malloc(sizeof(int));
+    cout << "Sorteando os números...";
+    for (int i = 0; i < max; i++){
+        int x = rand() % 100 + 1;
+        ptr[i] = x;
+        int* temp = (int*) realloc(ptr,(i+2)*sizeof(int));
+        ptr = temp;
+    }
+    cout << "...[ok]" << endl;
+    double media = 0;
+    for (int i = 0; i<max; i++){
+        media += ptr[i];
+    }
+    media = media/max;
+    cout <<"Media: "<< media << endl;
+    return 0;
+}
